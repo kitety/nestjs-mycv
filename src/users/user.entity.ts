c316bfba6d379bd1b00f8e7ad87483999ab96d08
kitety <aizaizuori@foxmail.com>
2022-06-19 23:14:22 +0800

    feat: update records

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,20 @@ export class User {
 
   @Column()
   password: string;
+
+  // 额外逻辑，自动执行
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id ', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id ', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id ', this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // 引入  Repository
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    // 先创建，再保存
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+    // bad this.repo.save({ email, password })
+    // 那为什么要先创建instance呢，因为instance可以有一些其他的逻辑，比如校验和限制
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      return new Error('User not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      return new Error('User not found');
+    }
+    return this.repo.remove(user);
+  }
+}
