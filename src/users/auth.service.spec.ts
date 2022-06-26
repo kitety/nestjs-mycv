e1a91a7827e01713190edce73a519d9557e883bf
kitety <aizaizuori@foxmail.com>
2022-06-26 21:42:47 +0800

    feat: test signup,p90

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,25 +1,38 @@
-import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { Test } from '@nestjs/testing';
 import { User } from './user.entity';
 
-it('can create an instance of auth service', async () => {
-  // create a fake copy of the user service
-  const fakeUserService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
-  // di
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUserService,
-      },
-    ],
-  }).compile();
-  const service = module.get(AuthService);
-  expect(service).toBeDefined();
+describe('AuthService', () => {
+  let service: AuthService;
+  // 每次运行都重新执行
+  beforeEach(async () => {
+    // create a fake copy of the user service
+    const fakeUserService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+    // di
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+        },
+      ],
+    }).compile();
+    service = module.get(AuthService);
+  });
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+  it('creates a new user with salted and hashede password', async () => {
+    const user = await service.signup('1@qq.com', 'pwd');
+    expect(user.password).not.toEqual('pwd');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
 });
