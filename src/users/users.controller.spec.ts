53251ea7a94d0a8391b63f955ceca64fe8d837b5
kitety <aizaizuori@foxmail.com>
2022-07-06 22:08:51 +0800

    feat: test controller end

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -33,8 +33,14 @@ describe('UsersController', () => {
       // async update(id: number, attrs: Partial<User>): Promise<User> {},
     };
     fakeAuthService = {
-      async signup(email: string, password: string): Promise<User> {},
-      async signin(email: string, password: string): Promise<User> {},
+      async signin(email: string, password: string): Promise<User> {
+        return Promise.resolve({
+          id: 1,
+          email,
+          password,
+        } as User);
+      },
+      // async signin(email: string, password: string): Promise<User> {},
     };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -55,5 +61,33 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  // test 不能处理装饰器
+  it('findAllUser returns a list with the given email', async () => {
+    const users = await controller.findAllUsers('1@qq.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('1@qq.com');
+  });
+  it('findOne returns a user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+    expect(user.id).toEqual(1);
+  });
+  it('findOne returns an error with the not given id', async () => {
+    const id = '11';
+    try {
+      await controller.findUser(id);
+    } catch (e) {
+      expect(e.message).toEqual('User ' + id + ' is  not found');
+    }
+  });
+  it('should signin updates Session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'user@example.com', password: 'password' },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(user.id).toEqual(session.userId);
   });
 });
